c9040bed8401af187d2c98ce09cd3bcd33a15205
arberzylyftariholberton <12033@holbertonstudents.com>
2025-11-22 19:15:18 +0100

    Trying to solve the problem

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
-"""A script that provides some stats about Nginx logs stored in MongoDB"""
+"""Display summarized statistics from Nginx logs stored in MongoDB."""
 
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    """
-    Documentation
+def summarize_nginx_logs(nginx_coll):
+    """ Function that prints total logs, method counts
+    and GET /status hits.
     """
 
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    nginx = db.nginx
+    total_entries = nginx_coll.count_documents({})
+    print(f"{total_entries} logs")
 
-    total_logs = nginx.count_documents({})
-    print(f"{total_logs} logs")
     print("Methods:")
+    methods_to_check = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for m in methods_to_check:
+        num_requests = nginx_coll.count_documents({"method": m})
+        print(f"\tmethod {m}: {num_requests}")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    status_hits = nginx_coll.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_hits} status check")
 
-    status_check = nginx.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_collection = client.logs.nginx
+    summarize_nginx_logs(nginx_collection)
